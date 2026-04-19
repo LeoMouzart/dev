53eb26bba3023cfebb89a3d865d27ec971efedbf
Leonardo Mouzart <leonardomouzart@gmail.com>
2026-04-19 16:52:42 -0300

    Atualização de estudos em C#

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Arrays;
+﻿using System.Runtime.InteropServices;
+
+namespace Arrays;
 
 class Program
 {
@@ -8,11 +10,24 @@ class Program
 
         var meuArray = new int[5];
 
-        Console.WriteLine("Digite o valor da posição 1");
-        meuArray[0] = int.Parse(Console.ReadLine());
+
+        for(var i =0; i < meuArray.Length; i++)
+        {
+            
+        Console.WriteLine($"Digite o valor da posição {i}");
+        meuArray[i] = int.Parse(Console.ReadLine());
+        }
 
 
-        Console.WriteLine(meuArray[0]);
+        for(var i =0; i<meuArray.Length; i++)
+        {
+            Console.WriteLine($"Posição {i}: {meuArray[i]} ");
+        }
+
+
+
+        var total = meuArray.Sum();
+        Console.WriteLine($"A soma dos valores é: {total}");
 
     }
 }
