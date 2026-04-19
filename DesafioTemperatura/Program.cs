981ae60ce472b309f885c06b9f22cc2c29c8bccb
Leonardo Mouzart <leonardomouzart@gmail.com>
2026-04-19 16:53:02 -0300

    "Atualização de estudos"

---
+++ DesafioTemperatura/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics.Contracts;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+
+namespace DesafioTemperatura;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+
+        var temperaturas = new double[7];
+        double total = 0;
+        
+        
+       for (var i = 0;i< temperaturas.Length; i++)
+        {
+            Console.WriteLine($"Digite a temperatura do {i+1}º dia da Semana: ");
+            temperaturas[i] = double.Parse(Console.ReadLine());
+
+            total += temperaturas[i];
+
+            Console.WriteLine($"A soma até o momento é: {total:F2}");
+
+        }
+
+        var soma = temperaturas.Sum();
+        var media = soma / temperaturas.Length;
+        var maior = temperaturas.Max();
+        var menor = temperaturas.Min();
+
+
+        RelatorioTemperaturas(media,maior,menor);
+    }
+
+    static void RelatorioTemperaturas (double media, double maior, double menor)
+    {
+        Console.WriteLine("------------------- Relatorio Semanal ---------------------");
+        Console.WriteLine($"Maior Temperatura: {maior:F2}ºC\nMenor Temperatura {menor:F2}ºC\nMédia de Temperatura: {media:F2}ºC");
+        Console.WriteLine("--------------------------------------------------------------------");
+
+    }
+}
---
+++ DesafioTemperatura/relatorio.md
@@ -0,0 +1,16 @@
+
+🏆 Desafio: O Analista de Temperaturas
+Você foi contratado para criar um pequeno sistema para uma estação meteorológica. O objetivo é ler as temperaturas registradas durante uma semana (7 dias) e gerar um relatório básico.
+
+O que o programa deve fazer:
+Entrada: Criar um array de double com tamanho 7.
+
+Coleta: Usar um laço (for) para pedir ao usuário que digite a temperatura de cada um dos 7 dias.
+
+Processamento:
+
+Calcular a Média das temperaturas da semana.
+
+Encontrar a Maior temperatura registrada.
+
+Encontrar a Menor temperatura registrada.
